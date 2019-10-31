804681ebbc7c204234da4611e13f9e8d1366c9ad
Kiril Bonduryansky <55139428+airlivered@users.noreply.github.com>
2019-11-01 01:25:54 +0200

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,7 +1,13 @@
 print("Бондурянський Кирило Юрійович \nЛабораторна робота №1 \nВаріант 1 \nПерше завдання \nПереведення кута з радіанної міри в градусну")
-x = float(input("Введіть кут радіанної міри: "))
-yd = x*57.29577
+def is_num(symbol):
+    if re.search(r'^\d+$', symbol):
+        return True
+angle = input("Введіть кут радіанної міри: ")
+while not is_num(angle):
+    angle = input("Введіть кут радіанної міри: ")
+angle = float(angle)
+yd = angle*57.29577
 deg = int(yd)
 min = int((yd - deg)*60)
 sec = int(((yd - deg)*60 - min)*60)
-print("Кут " + str(x) + " радіан рівний " + str(deg) + " градусам " + str(min) + " хвилинам і " + str(sec) +" секундам")
+print("Кут " + str(angle) + " радіан рівний " + str(deg) + " градусам " + str(min) + " хвилинам і " + str(sec) +" секундам")
